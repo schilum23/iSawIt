07586bd8af760f160d3748fbeb23f529a7b75f9a
Oliver Rosner <Schilum23@Olivers-MacBook-Pro.local>
2014-09-06 20:44:15 +0200

    Initial Commit

---
+++ iSawIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iSawIt
+//
+//  Created by Oliver Rosner on 06.09.14.
+//  Copyright (c) 2014 Schilum23. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
